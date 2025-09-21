c2973f7400b5a0be1d02d434f621f37c96aa58d1
KennyDevz <kenneth.kenn2004@gmail.com>
2025-09-21 14:59:14 +0800

    feature(routes): complete app setup

--- map_routes/routes/views.py
+++ map_routes/routes/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Location
+from .forms import LocationForm
+import json
 
-# Create your views here.
+def map_view(request):
+    if request.method == "POST":
+        form = LocationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("map")  # refresh after saving
+    else:
+        form = LocationForm()
+
+    locations = list(Location.objects.values("name", "description", "latitude", "longitude"))
+    context = {
+        "form": form,
+        "locations": json.dumps(locations),
+    }
+    return render(request, "routes/routes.html", context)
